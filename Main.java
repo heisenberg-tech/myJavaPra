ca07bf1938add13c5b7047edaab766244003f4b7
AL_HE <al_he@AL-HEnoMacBook-Air.local>
2021-02-07 12:05:39 +0900

    first upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+package mydev;
+
+public class Main {
+    public static void main(String[] args) {
+        /* Math
+        System.out.println((int) Math.pow(5, 2));
+        System.out.println(Math.abs(-55));
+        System.out.println(Math.max(6, 10));
+        System.out.println(Math.min(6, 10));
+        System.out.println(Math.round(2.2));
+        */
+
+        /* if statement
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("adult");
+        } else if (age >= 16 && age < 18){
+            System.out.println("almost adult");
+        } else {
+            System.out.println("no adult");
+        }
+        */
+
+        /* Switch statement
+        String gender = "FEMALE";
+        switch (gender.toUpperCase()) {
+            case "FEMALE":
+                System.out.println("the gender is FEMALE");
+                break;
+            case "MALE":
+                System.out.println("the gender is MALE");
+                break;
+            default:
+                System.out.println("the gender is not recognized!");
+                break;
+        }
+        */
+
+    }
+}
